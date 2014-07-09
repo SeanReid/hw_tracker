fc164e8e2a16e9623c2cea6bf189e6ca885a4a55
Sean Reid <sreid757@gmail.com>
2014-07-08 23:04:32 -0500

    rmvd debug header and commented new SA's

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,8 @@
 class Assignment < ActiveRecord::Base
   has_many :student_assignments
   has_many :students, through: :student_assignments
+
+  def self.active
+    where("assigned <= ?", Date.today)
+  end
 end
--- app/models/student_assignment.rb
+++ app/models/student_assignment.rb
@@ -1,4 +1,14 @@
 class StudentAssignment < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :student
+
+  validates :student, presence: true
+  validates :assignment, presence: true
+  validates :on_time, presence: true
+  validates :grade, presence: true
+  validates :github_url, presence: true
+
+  before_validation on: :create do
+    self.on_time = Date.today <= assignment.due
+  end
 end
